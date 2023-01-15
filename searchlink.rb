c921096493baddd4f93e82844e156b450eff2b46
Brett Terpstra <me@brettterpstra.com>
2023-01-15 06:39:27 -0600

    preparing for 2.3.4 release

--- searchlink.rb
+++ searchlink.rb
@@ -2,7 +2,7 @@
 # encoding: utf-8
 
 SILENT = ENV['SL_SILENT'] =~ /false/i ? false : true
-VERSION = '2.3.3'
+VERSION = '2.3.4'
 
 # SearchLink by Brett Terpstra 2015 <http://brettterpstra.com/projects/searchlink/>
 # MIT License, please maintain attribution
