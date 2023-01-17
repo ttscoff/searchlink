1b1fb52a3bed72234f838b00861c844b69c9ee55
Brett Terpstra <me@brettterpstra.com>
2023-01-17 06:06:14 -0600

    Add !file search
    - NEW: `!file SPOTLIGHT_QUERY` will search for a local file and output a `file://` link with the filename as title

--- searchlink.rb
+++ searchlink.rb
@@ -707,11 +707,149 @@ class SearchLink
       ['hf[hb]', 'Firefox [history, bookmarks]'],
       ['he[hb]', 'Edge [history, bookmarks]'],
       ['hb[hb]', 'Brave [history, bookmarks]'],
-      ['te', 'Twitter embed']
+      ['te', 'Twitter embed'],
+      ['file', 'Local file:// link (Spotlight)']
     ]
     out = ''
     searches.each { |s| out += "!#{s[0]}#{spacer(s[0])}#{s[1]}\n" }
     out
+  end
+
+  def best_search_match(term)
+    searches = all_possible_searches.dup
+    searches.select do |s|
+      s.matches_score(term, separator: '', start_word: false) > 8
+    end
+  end
+
+  def did_you_mean(term)
+    matches = best_search_match(term)
+    matches.empty? ? '' : ", did you mean #{matches.map { |m| "!#{m}" }.join(', ')}?"
+  end
+
+  def all_possible_searches
+    %w[
+      @f
+      @i
+      @l
+      @m
+      @t
+      a
+      amalb
+      amalbe
+      amart
+      amarte
+      ampod
+      ampode
+      amsong
+      amsonge
+      b
+      ddg
+      def
+      file
+      g
+      h
+      ha
+      hab
+      habh
+      hah
+      hahb
+      hb
+      hbb
+      hbbh
+      hbh
+      hbhb
+      hc
+      hcb
+      hcbh
+      hch
+      hchb
+      he
+      heb
+      hebh
+      heh
+      hehb
+      hf
+      hfb
+      hfbh
+      hfh
+      hfhb
+      hs
+      hsb
+      hsbh
+      hsh
+      hshb
+      ialb
+      ialbe
+      iart
+      iarte
+      imov
+      ipod
+      ipode
+      isong
+      isonge
+      itu
+      itud
+      lalb
+      lalbe
+      lart
+      larte
+      lpod
+      lpode
+      lsong
+      lsonge
+      mas
+      masd
+      pb
+      r
+      s
+      sp
+      spell
+      te
+      tmdb
+      tmdba
+      tmdbm
+      tmdbt
+      wiki
+      yt
+      yte
+      z
+      zero
+    ].concat(@cfg['custom_site_searches'].keys)
+  end
+
+  def valid_searches
+    [
+      'h(([scfabe])([hb])?)*',
+      'a',
+      'imov',
+      'g',
+      'ddg',
+      'z(ero)?',
+      'b',
+      'wiki',
+      'def',
+      'masd?',
+      'itud?',
+      'tmdb[amt]?',
+      's',
+      '(i|am|l)(art|alb|song|pod)e?',
+      '@[tfilm]',
+      'r',
+      'sp(ell)?',
+      'pb',
+      'yte?',
+      'te',
+      'file'
+    ]
+  end
+
+  def valid_search?(term)
+    valid = false
+    valid = true if term =~ /^(#{valid_searches.join('|')})$/
+    valid = true if @cfg['custom_site_searches'].keys.include? term
+    notify("Invalid search#{did_you_mean(term)}", term) unless valid
+    valid
   end
 
   def spacer(str)
@@ -1673,139 +1811,11 @@ APPLESCRIPT
     false
   end
 
-  def best_search_match(term)
-    searches = all_possible_searches.dup
-    searches.select do |s|
-      s.matches_score(term, separator: '', start_word: false) > 8
-    end
-  end
+  def spotlight(query)
+    res = `mdfind '#{query}' 2>/dev/null|head -n 1`
+    return [false, query] if res.strip.empty?
 
-  def did_you_mean(term)
-    matches = best_search_match(term)
-    matches.empty? ? '' : ", did you mean #{matches.map { |m| "!#{m}" }.join(', ')}?"
-  end
-
-  def all_possible_searches
-    %w[
-      h
-      hs
-      hsh
-      hshb
-      hsbh
-      hsb
-      hc
-      hch
-      hcb
-      hchb
-      hcbh
-      hf
-      hfh
-      hfb
-      hfhb
-      hfbh
-      he
-      heh
-      heb
-      hehb
-      hebh
-      hb
-      hbh
-      hbb
-      hbhb
-      hbbh
-      ha
-      hah
-      hab
-      habh
-      hahb
-      a
-      imov
-      g
-      ddg
-      z
-      zero
-      b
-      wiki
-      def
-      mas
-      masd
-      itu
-      itud
-      tmdb
-      tmdba
-      tmdbm
-      tmdbt
-      s
-      iart
-      ialb
-      isong
-      ipod
-      iarte
-      ialbe
-      isonge
-      ipode
-      lart
-      lalb
-      lsong
-      lpod
-      larte
-      lalbe
-      lsonge
-      lpode
-      amart
-      amalb
-      amsong
-      ampod
-      amarte
-      amalbe
-      amsonge
-      ampode
-      @t
-      @f
-      @i
-      @l
-      @m
-      r
-      sp
-      spell
-      pb
-      yt
-      yte
-      te
-    ].concat(@cfg['custom_site_searches'].keys)
-  end
-
-  def valid_searches
-    [
-      'h(([scfabe])([hb])?)*',
-      'a',
-      'imov',
-      'g',
-      'ddg',
-      'z(ero)?',
-      'b',
-      'wiki',
-      'def',
-      'masd?',
-      'itud?',
-      'tmdb[amt]?',
-      's',
-      '(i|am|l)(art|alb|song|pod)e?',
-      '@[tfilm]',
-      'r',
-      'sp(ell)?',
-      'pb',
-      'yte?',
-      'te'
-    ]
-  end
-
-  def valid_search?(term)
-    valid = false
-    valid = true if term =~ /^(#{valid_searches.join('|')})$/
-    valid = true if @cfg['custom_site_searches'].keys.include? term
-    notify("Invalid search#{did_you_mean(term)}", term) unless valid
-    valid
+    ["file://#{res.strip}", File.basename(res)]
   end
 
   def search_arc_history(term)
@@ -2796,6 +2806,9 @@ APPLESCRIPT
       end
 
       url, title = social_handle('m', search_terms)
+      link_text = title
+    when /^file$/
+      url, title = spotlight(search_terms)
       link_text = title
     when /^sp(ell)?$/ # replace with spelling suggestion
       res = spell(search_terms)
