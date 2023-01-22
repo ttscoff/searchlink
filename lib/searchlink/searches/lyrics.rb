3e084f4ce6127989827ec917b4062ce0752aa557
Brett Terpstra <me@brettterpstra.com>
2023-01-22 06:17:58 -0600

    More example plugin comments

--- lib/searchlink/searches/lyrics.rb
+++ lib/searchlink/searches/lyrics.rb
@@ -1,15 +1,19 @@
+# Always start with module SL
 module SL
-  # Unique class name
+  # Give it a unique class name
   class LyricsSearch
     class << self
+      # Settings block is required with `trigger` and `searches`
       def settings
         {
-          # A regular expression that will trigger this plugin when used with
-          # a bang. The one below will trigger on !lyrics or !lyricse.
+          # `trigger` is A regular expression that will trigger this plugin
+          # when used with a bang. The one below will trigger on !lyrics or
+          # !lyricse.
           trigger: 'lyrics?e?',
-          # Every search that the plugin should do should be individually
+          # Every search that the plugin should execute should be individually
           # listed and described in the searches array. This is used for
-          # completion and help generation.
+          # completion and help generation. Do not include the bang (!) in the
+          # search keyword.
           searches: [
             ['lyric', 'Song Lyrics Search'],
             ['lyrice', 'Song Lyrics Embed']
@@ -17,8 +21,8 @@ module SL
         }
       end
 
-      # Every plugin must contain a #search method that takes 3 arguments,
-      # search_type, search_terms, and link_text.
+      # Every plugin must contain a #search method that takes 3 arguments:
+      #
       # - `search_type` will contain the !search trigger that was used (minus the !)
       # - `search_terms` will include everything that came after the !search
       # - `link_text` will contain the text that will be used for the linked
@@ -31,13 +35,26 @@ module SL
           url, title = SL.ddg("site:genius.com #{search_terms}", link_text)
           if url
             title = get_lyrics(url)
+            # To return an embed, set url (first parameter in the return
+            # array) to 'embed', and put the embed contents in the second
+            # parameter.
             title ? ['embed', title, link_text] : false
           else
+            # Use `SL#add_error(title, text)` to add errors to the HTML
+            # report. The report will only be shown if errors have been added.
             SL.add_error('No lyrics found', "Song lyrics for #{search_terms} not found")
             false
           end
         else
-          url, title = SSL.ddg("site:genius.com #{search_terms}", link_text)
+          # You can perform a DuckDuckGo search using SL#ddg, passing the
+          # search terms and link_text. It will return url, title, and
+          # link_text. SL#ddg will add its own errors, and if it returns false
+          # that will automatically be tested for, no additional error
+          # handling is required.
+          url, title, link_text = SL.ddg("site:genius.com #{search_terms}", link_text)
+          # Always return an array containing the resulting URL, the title,
+          # and the link_text variable that was passed in, even if it's
+          # unmodified.
           [url, title, link_text]
         end
       end
@@ -45,6 +62,8 @@ module SL
       # Any additional helper methods can be defined after #search
       def get_lyrics(url)
         if SL::URL.valid_link?(url)
+          # You can use Ruby's net/http methods for retrieving pages, but
+          # `curl -SsL` is faster and easier.
           body = `curl -SsL #{url}`
 
           matches = body.scan(%r{class="Lyrics__Container-.*?>(.*?)</div><div class="RightSidebar})
