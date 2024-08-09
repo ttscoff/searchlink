54ae0f6c78bc21d3e2b34510668feda345c164ac
Brett Terpstra <brett.terpstra@oracle.com>
2024-08-09 06:49:44 -0500

    add documentation

--- lib/searchlink/array.rb
+++ lib/searchlink/array.rb
@@ -1,5 +1,10 @@
 # Array helpers
 class ::Array
+  # 
+  # This method takes an array and returns the longest element of that array.
+  #
+  # @return [String] Longest string in array
+  #
   def longest_element
     group_by(&:size).max.last[0]
   end
